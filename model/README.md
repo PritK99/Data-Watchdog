3625a29b319f83650b5e2b86e74e25ab76860585
Prit Kanadiya <103832825+PritK99@users.noreply.github.com>
2024-09-07 11:19:36 +0530

    Adding PII detection for unstructured textual data

---
+++ model/README.md
@@ -0,0 +1,17 @@
+# Research
+
+For PII data detection, there can be two ways PII data might be available in databases. One is in structured format, that is the column names, JSON or SQL dumps. For eg. a csv having column of date of birth. Other can be textual fields. Here, we need to identify PII data in the text. This includes .pdf, .txt, .log etc. Unstructured data also includes multimedia like video and images.
+
+## Structured Data
+
+Pending work.
+
+## Unstructured Data
+
+### Textual files
+
+Here we deal with types such as .pdf, .txt, .log etc. We use NER and regex to identify PII among them. 
+
+### Multimedia
+
+This is a future scope for now.
---
+++ model/pii_core.py
@@ -0,0 +1,62 @@
+import re
+import logging
+
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import pipeline
+
+# Storing the results in a log file when using standalone
+# logging.basicConfig(filename='pii_detection.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Loading the tokenizer and model
+tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
+model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
+ner = pipeline("ner", model=model, tokenizer=tokenizer)
+
+# Patterns for PII used in step 2
+patterns = {
+        "EMAIL": r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}',
+        "IP": r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b',
+        "PHONE": r'\b(?:\+91|91)?[789]\d{9}\b',
+        "AADHAR": r'\b\d{4} \d{4} \d{4}\b',
+        "PAN": r'\b[A-Z]{5}\d{4}[A-Z]\b',
+        "PINCODE": r'\b\d{6}\b'
+    }
+
+def detect_pii_from_string(input_str):
+    pii = []
+
+    # Step 1: Tokenizing the input text on space
+    # We do not use the tokenizer here because of its subtokenizing approach
+    tokenized_output = input_str.split()
+
+    # Step 2: Performing Regex for different types of PII
+    # This step handles EMAIL, IP, PHONE, AADHAR, PAN, PINCODE
+    for key, pattern in patterns.items():
+        matches = re.findall(pattern, input_str)
+        for match in matches:
+            pii.append([match, key])
+
+    # Step 3: Performing NER and processing the result
+    # This step handles PER, ORG, LOC and MISC
+    ner_results = ner(input_str)
+    for instance in ner_results:
+        word = instance["word"]
+        entity = instance["entity"]
+
+        # This happens because of the subtokenization technique used by tokenizer
+        if (word[0] == "#"):
+            pii[-1][0] += word[2:]
+        # This allows us to handle words like Vice City
+        elif (entity[0] == "I" and pii[-1][1] == entity[2:]):
+            word = " " + word
+            pii[-1][0] += word
+        # Appending new NERs
+        else:
+            pii.append([word, entity[2:]])
+
+    # Logging the results when using standalone
+    # logging.info(f'Detected PII: {pii}')
+
+    return pii
+
+# detect_pii_from_string("I live in Jelum Towers, Vice City, 400809, and I go by Sonny. I had an amazing drive through Vice City's neon-lit streets before heading to the storied Malibu Club for an exciting night of music that will never be forgotten. I'm constantly in awe of the city's breathtaking skyline and lively nightlife. Lance Vance, a buddy of mine, will see me shortly to discuss a mission. He is employed with The Dockyards. Using the IP address 172.182.99.1, he emailed me about the mission using the email address lancevance@gmail.com. A message from +919999999999 reached me as well. It appears to be authentic.")
